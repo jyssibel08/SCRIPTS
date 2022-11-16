2f30ce1a5debb33ee312a52b11d9592e11965437
jyssibel08 <95346666+jyssibel08@users.noreply.github.com>
2022-11-17 02:23:31 +0800

    Update Ubuntu-VPS-Installer.sh

--- Ubuntu-VPS-Installer.sh
+++ Ubuntu-VPS-Installer.sh
@@ -1,3 +1,24 @@
+clear
+#fonts color
+Green="\033[32m"
+Red="\033[31m"
+Yellow="\033[33m"
+GreenBG="\033[42;37m"
+Purple="\033[0;35m"
+RedBG="\033[41;37m"
+Font="\033[0m"
+PurpleBG="\033[45;37m"
+YellowBG="\033[43m"
+    echo -e "${Purple}—————————————— XAMJYSS143 Modified Script ——————————————${Font}"
+    echo -e "${Yellow}Credits to:${Font} ${Green}Bonveio(Bon-Chan)${Font}"
+    echo -e ""
+    echo -e "${Yellow}Note: Please Subscribe to my Youtube Channel ${Red}( https://www.youtube.com/channel/UC7LIFRhsjIi9tXc2MWUS6nw )${font} ${Yellow}and Watch the Tutorial for this scipt's password.${Font}"
+    echo -e ""
+read -rp "Please Enter Script Password：" menu_num1
+if [ $menu_num1 == 'UC7LIFRhsjIi9tXc2MWUS6nw' ]
+then
+
+
 echo "deb http://security.ubuntu.com/ubuntu xenial-security main" >> /etc/apt/sources.list
 sudo apt update
 sudo apt install libssl1.0.0
