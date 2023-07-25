3cdf9fdaaecc84afe5b0d7388df0d005ee5b9e5b
jyssibel08 <95346666+jyssibel08@users.noreply.github.com>
2023-07-25 14:55:25 +0800

    Update services.sh

--- services.sh
+++ services.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 for service in $(systemctl list-units --type=service --state=running | awk '/.service/ && !/dbus/ {print $1}'); do
     echo "Restarting $service"
     sudo systemctl restart $service && echo "$service restarted successfully" || echo "Failed to restart $service"
