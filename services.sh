08cff7fabc5953125d13bfbc165ba384824ef954
jyssibel08 <95346666+jyssibel08@users.noreply.github.com>
2023-07-29 15:38:12 +0800

    Update services.sh

--- services.sh
+++ services.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for service in $(systemctl list-units --type=service --state=running | awk '/.service/ && !/dbus/ {print $1}'); do
+for service in $(systemctl list-units --type=service --state=running | awk '/.service/ && !/dbus/ && !/openvpn/ {print $1}'); do
     echo "Restarting $service"
     sudo systemctl restart $service && echo "$service restarted successfully" || echo "Failed to restart $service"
 done
